5ef229e536282cd6a0d7cb9d9cec0dc9a115d9b4
ma'sirfani <muhammadwalle123@gmail.com>
2020-07-31 10:19:43 +0700

    tambah link jatimpark3

--- js/crud_firebase.js
+++ js/crud_firebase.js
@@ -13,32 +13,32 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
-// CREATE
-firebase.database().ref('users/' + userId).set({
-  username : name,
-  email : email,
-  profile_picture : profile_picture,
-});
+// // CREATE
+// firebase.database().ref('users/' + userId).set({
+//   username : name,
+//   email : email,
+//   profile_picture : profile_picture,
+// });
 
-// READ
-var comentsRef = firebase.database().ref('post-comments' + postId);
-commentsRef.on('child_added', function(data){
-  addCommentElement(postElement, data.key, data.val().text, data.val().author);
-});
+// // READ
+// var comentsRef = firebase.database().ref('post-comments' + postId);
+// commentsRef.on('child_added', function(data){
+//   addCommentElement(postElement, data.key, data.val().text, data.val().author);
+// });
 
-commentsRef.on('child_changed', function(data){
-  addCommentValues(postElement, data.key, data.val().text, data.val().author);
-});
+// commentsRef.on('child_changed', function(data){
+//   addCommentValues(postElement, data.key, data.val().text, data.val().author);
+// });
 
-commentsRef.on('child_removed', function(data){
-  deleteComment(postElement, data.key);
-});
+// commentsRef.on('child_removed', function(data){
+//   deleteComment(postElement, data.key);
+// });
 
-// UPDATE
-var updates = {};
-updates['/post/' + newPostKey] = postData;
-updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-return firebase.database().ref().update(updates);
+// // UPDATE
+// var updates = {};
+// updates['/post/' + newPostKey] = postData;
+// updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+// return firebase.database().ref().update(updates);
 
-// DELETE
-firebase.database().ref().remove();
+// // DELETE
+// firebase.database().ref().remove();
